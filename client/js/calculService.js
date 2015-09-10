f6d4c06c9cdb8b26bc778cd8e4e7c82dfb05e9f7
Jean-François Fourmond <jffourmond@gmail.com>
2015-09-10 16:52:19 +0200

    Modif url serveur

--- client/js/calculService.js
+++ client/js/calculService.js
@@ -5,7 +5,7 @@ app.service('calculService', ['$http', function ($http) {
     this.posterMessage = function (token, channel, username, iconUrl, text) {
 		   var req = {
 			 method: 'POST',
-			 url: 'http://localhost:3000',
+			 url: 'http://51.254.102.153:3000',
 			 headers: {
 			   'Content-Type': 'application/json'
 			 },
