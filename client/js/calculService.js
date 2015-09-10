088c2b8b8be50f772bcbbbd00bb4c068def0aa70
Jean-François Fourmond <jffourmond@gmail.com>
2015-09-10 17:27:49 +0200

    Modif POST angular

--- client/js/calculService.js
+++ client/js/calculService.js
@@ -3,22 +3,14 @@
 app.service('calculService', ['$http', function ($http) {
 
     this.posterMessage = function (token, channel, username, iconUrl, text) {
-		   var req = {
-			 method: 'POST',
-			 url: 'http://51.254.102.153:3000/slack',
-			 headers: {
-			   'Content-Type': 'application/json'
-			 },
-			 data: { 
+
+		$http.post('http://51.254.102.153:3000/slack', { 
 				token: token, 
 				channel: channel, 
 				username: username, 
 				icon_url: iconUrl,
 				text: text			
-			 }
-			}
-
-		$http.post(req);
+			 });
     };
 
    
