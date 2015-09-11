80b1f073bc107332892a40950c7590140f80edf3
Jean-François Fourmond <jffourmond@gmail.com>
2015-09-11 08:24:34 +0200

    Refactoring

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/home/jffourmond/slack-puppets/index.html');
+
+});
+
+app.post('/slack', function (req, res) {
+
+  console.log('url : ' + req.body.token);
+  request.post({
+    url:     'https://slack.com/api/chat.postMessage',
+    form:    {
+	token :  req.body.token, 
+        username :  req.body.username, 
+        channel : req.body.channel,
+        text : req.body.text,
+        icon_url : req.body.icon_url
+    }
+  }, 
+  function(error, response, body){
+   console.log(body);
+  });
+
+res.send('Slackkk!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
