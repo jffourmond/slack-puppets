67e554a61550b3800d3a2e77b32c5fe81a7f2944
Jean-François Fourmond <jffourmond@gmail.com>
2015-10-17 12:51:33 +0200

    Ajout de logs

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.static('client'));
 app.post('/messages', function (req, res) {
 
   var body = req.body;
-  console.log('url : ' + body.token);
+  var dateTime = new Date().toISOString();
+  var ip = req.connection.remoteAddress;
+  console.log('Message received at %s from IP %s : username = %s and channel = %s', dateTime, ip, body.username, body.channel);
+  
   request.post({
     url:     'https://slack.com/api/chat.postMessage',
     form:    {
@@ -25,7 +28,7 @@ app.post('/messages', function (req, res) {
   function(error, response, bodyString){
     var status = response.statusCode;
     var body = JSON.parse(bodyString);
-    console.log('STATUS : ' + status + ' / BODY : ' + body.ok);
+    console.log('Status : ' + status + ' / success : ' + body.ok);
     if (body.ok === false){
      console.log('ERROR : ' + bodyString);
      res.status(500).send(body.error);
@@ -36,9 +39,8 @@ app.post('/messages', function (req, res) {
 
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
